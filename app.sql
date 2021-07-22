156bcb1e3cb96edd68b084ff4e31c251cb108399
“WhoIsKeyserSoze” <gaby.foix@gmail.com>
2021-07-22 15:48:39 +0200

    Added orderUser.js fully working.

--- app.sql
+++ app.sql
@@ -10,6 +10,15 @@ CREATE TABLE products (
   primary key (item_id)
 );
 
+CREATE TABLE catalogue (
+	item_id INT AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(100),
+  price INT,
+  primary key (item_id)
+);
+
+
+
 insert into products (product_name, price, quantity)
 values ('Cereales Crunch',3.50, 1);
 
@@ -33,6 +42,53 @@ values ('Pates 500g',1.50, 4);
 
 insert into products (product_name, price, quantity)
 values ('Sauce tomate',2.50, 2);
+
+
+
+insert into catalogue (product_name, price)
+values ('Cereales Crunch',3.50);
+
+insert into catalogue (product_name, price)
+values ('biscuits apéritifs',2.50);
+
+insert into catalogue (product_name, price)
+values ('sangria 1.5L',4.50);
+
+insert into catalogue (product_name, price)
+values ('Pot de Nutella',3.50);
+
+insert into catalogue (product_name, price)
+values ('Café',3.20);
+
+insert into catalogue (product_name, price)
+values ('Gel douche',1.50);
+
+insert into catalogue (product_name, price)
+values ('Dentifrice',2.50);
+
+insert into catalogue (product_name, price)
+values ('Brosse à dents',2.50);
+
+insert into catalogue (product_name, price)
+values ('Shampoing',3.50);
+
+insert into catalogue (product_name, price)
+values ('Riz 1kg',1.50);
+
+insert into catalogue (product_name, price)
+values ('Pommes de terre 2kg',2.40);
+
+insert into catalogue (product_name, price)
+values ('Bananes',3.50);
+
+insert into catalogue (product_name, price)
+values ('Frites',2.70);
+
+insert into catalogue (product_name, price)
+values ('Ketchup',2.50);
+
+insert into catalogue (product_name, price)
+values ('Lait 6L',5);
 
 
 select * from products;
--- orderUser.js
+++ orderUser.js
@@ -5,29 +5,34 @@ var Table = require("cli-table3");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "toor",
-    database: "CLI_COM"
+    user: "jean",
+    password: "password",
+    database: "CLI_COM",
+    insecureAuth : true
 });
 
-connection.connect(function (error) {
-    if (error) throw error;
+
+connection.connect(function(err){
+    if(err) throw err;
     console.log("Connected as ID:", connection.threadId + "\n");
     menu();
 });
 
 
 function menu() {
-    console.log((chalk.red("Welcome to Order Management Interface!")))
+    console.log((chalk.red("Welcome to Participative Order Interface!")))
     inquirer.prompt({
         type: "list",
         message: "What would you like to do?",
         name: "choice",
-        choices: ["See the current Order", "Exit"]
+        choices: ["See the current Order", "Add items from catalogue", "Exit"]
     }).then(function (response) {
         switch (response.choice) {
             case "See the current Order":
                 orderItems();
+                break;
+            case "Add items from catalogue":
+                catalogueItems();
                 break;
             case "Exit":
                 connection.end();
@@ -36,6 +41,7 @@ function menu() {
         }
     })
 }
+
 
 function orderItems() {
     var query = "SELECT * FROM products";
@@ -60,17 +66,20 @@ function orderItems() {
     })
 }
 
+
+
 function orderManagement() {
     console.log((chalk.green("Order Management")))
     inquirer.prompt({
         type: "list",
         message: "What would you like to do?",
         name: "choice",
-        choices: ["Modify order", "Exit"]
+        choices: ["Modify items added by me", "Exit"]
     }).then(async function (response) {
         switch (response.choice) {
-            case "Modify order":
-                modifyOrder();
+            case "Modify items added by me":
+                console.log("Not Implemented / Maybe later")
+                connection.end();
                 break;
             case "Exit":
                 connection.end();
@@ -80,10 +89,34 @@ function orderManagement() {
     })
 }
 
-function modifyOrder(){
+
+function catalogueItems() {
+    var query = "SELECT * FROM catalogue";
+    connection.query(query, function (error, response) {
+        if (error) throw error;
+        var divider = "\n------------------------------------------------------------\n";
+        var greeting = "\n" + "Catalogue :" + "\n"
+        console.log(greeting);
+        var table = new Table({
+            head: ["ID", "Name", "Price"]
+        });
+        for (var i = 0; i < response.length; i++) {
+            table.push([
+                response[i].item_id,
+                response[i].product_name,
+                '$' + response[i].price
+            ]);
+        }
+        console.log(table.toString() + "\n");
+        addToOrder();
+    })
+}
+
+
+function addToOrder(){
     inquirer.prompt([{
         type: "input",
-        message: "Please enter the ID of the product you would like to modify.",
+        message: "Please enter the ID of the product you would like to add to the order.",
         name: "item_id",
         validate: function (value) {
             if (isNaN(value) === false) {
@@ -94,8 +127,8 @@ function modifyOrder(){
     },
     {
         type: "input",
-        message: "Press 1 for deleting product, 2 to change quantity",
-        name: "action",
+        message: "Enter quantity for selected item :",
+        name: "quantity",
         validate: function (value) {
             if (isNaN(value) === false) {
                 return true;
@@ -110,46 +143,20 @@ function modifyOrder(){
         default: true
     }])
     .then(function (answer) {
-      connection.query("SELECT * FROM products WHERE ?", {
+      var itemID = answer.item_id
+      connection.query("SELECT * FROM catalogue WHERE ?", {
           item_id: answer.item_id
       }, function (error, response) {
-          if(answer.action == 1){
-              console.log("\nYou have chosen to delete item : " + answer.item_id);
-              console.log("Processing ...");
-              var update = "DELETE FROM products WHERE item_id = " + answer.item_id;
-              connection.query(update, function (error, response) {
-                  if (error) throw error;
-                  console.log("-------------------------------")
-                  console.log("Item successfully deleted !")
-                  console.log("-------------------------------")
-                  setTimeout(orderItems, 1000);
-              })
-          }
-          else if (answer.action == 2){
-              console.log("\nYou have chosen to change quantity on item : " + answer.item_id);
-              console.log("Processing ...");
-              inquirer.prompt([{
-                  type: "input",
-                  message: "Enter new quantity",
-                  name: "newQ",
-                  validate: function (value) {
-                      if (isNaN(value) === false) {
-                          return true;
-                      }
-                      return false;
-                  }
-              }]).then(function (ans){
-                  var update = "UPDATE products SET quantity = " + ans.newQ + " WHERE item_id = " + answer.item_id;
-                  connection.query(update, function (error, response) {
-                      if (error) throw error;
-                      console.log("-------------------------------")
-                      console.log("Quantity successfully changed !")
-                      console.log("-------------------------------")
-                      setTimeout(orderItems, 1000);
-                  })
-              })
-
-          }
+            var name = "(SELECT product_name FROM catalogue WHERE item_id = " + itemID +")";
+            var price = "(SELECT price FROM catalogue WHERE item_id = "+ itemID +")";
+            var add = "INSERT INTO products (product_name, price, quantity) VALUES (" + name + " , " + price + " , " + answer.quantity + ")";
+            connection.query(add, function (error, response) {
+                if (error) throw error;
+                console.log("-------------------------------")
+                console.log("Item successfully added !")
+                console.log("-------------------------------")
+                setTimeout(catalogueItems, 1000);
+            })
 
         })
     });
